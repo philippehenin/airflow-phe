d1ea97738fd9c874264e65e7ad933eea90a6cd3e
Philippe Henin <philippehenin@gmail.com>
2024-10-29 18:38:44 +0100

    install cmd

---
+++ install-airflow.sh
@@ -0,0 +1,11 @@
+# !!! Attention.  No good with mini config 2 cpus 2 gb memory !!!
+# used 7 cpus 10gb memory
+
+helm repo add apache-airflow https://airflow.apache.org
+helm repo update apache-airflow
+helm upgrade --install airflow apache-airflow/airflow \
+  --namespace airflow \
+  --create-namespace \
+  --set-string "env[0].name=AIRFLOW__CORE__LOAD_EXAMPLES" \
+  --set-string "env[0].value=True" \
+  --timeout 20m0s --debug
