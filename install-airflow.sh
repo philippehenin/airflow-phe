53f3a3b0a8c5d49d840611cf03f3eec6d56aede0
Philippe Henin <philippehenin@gmail.com>
2024-10-29 18:43:18 +0100

    Comment

--- install-airflow.sh
+++ install-airflow.sh
@@ -1,5 +1,6 @@
 # !!! Attention.  No good with mini config 2 cpus 2 gb memory !!!
 # used 7 cpus 10gb memory
+#  sur XPS-13 i7 8gen, ca rammme a mort 80% cpu a l'instllc!!!
 
 helm repo add apache-airflow https://airflow.apache.org
 helm repo update apache-airflow
